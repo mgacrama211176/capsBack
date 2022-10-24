c426bd279e7c8cecbccbe16b38d8e2290ccf8e3e
MarlonG <mgacrama_ccs@uspf.edu.ph>
2022-10-24 15:20:15 +0800

    try again

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ dotenv.config();
 
 const PORT = process.env.PORT||4000;
 const connectDB = () => {
-  mongoose.connect(process.env.MONGODB)
+  mongoose.connect(process.env.MONGODB,{ useNewUrlParser: true })
     .then(() => {
       console.log("Database Connected");
     })
