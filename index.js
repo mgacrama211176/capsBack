0adf5f7c1cfa305f7f1d22ec7bfe5c3372df3bef
MarlonG <mgacrama_ccs@uspf.edu.ph>
2022-10-24 14:59:23 +0800

    again

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ dotenv.config();
 
 const PORT = process.env.PORT || 4000;
 const connectDB = () => {
-  mongoose.connect(process.env.MONGODB)
+  mongoose.connect(process.env.MONGODB, PORT)
     .then(() => {
       console.log("Database Connected");
     })
