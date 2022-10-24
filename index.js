233016335236b8dcfcc32782b35e6a5f5e672352
MarlonG <mgacrama_ccs@uspf.edu.ph>
2022-10-24 15:09:43 +0800

    Test again

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 dotenv.config();
 
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT||4000;
 const connectDB = () => {
   mongoose.connect(process.env.MONGODB)
     .then(() => {
