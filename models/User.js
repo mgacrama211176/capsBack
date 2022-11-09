3e4be5b568307c72b86035edca7ee1695b04594d
MarlonG <mgacrama_ccs@uspf.edu.ph>
2022-11-09 12:40:40 +0800

    changed default image

--- models/User.js
+++ models/User.js
@@ -26,7 +26,7 @@ const UserSchema = new mongoose.Schema(
     image: {
       type: String,
       default:
-        "https://media.npr.org/assets/img/2017/09/12/macaca_nigra_self-portrait-3e0070aa19a7fe36e802253048411a38f14a79f8-s1100-c50.jpg",
+        "https://www.kindpng.com/picc/m/451-4517876_default-profile-hd-png-download.png",
     },
     subscribers: {
       type: Number,
